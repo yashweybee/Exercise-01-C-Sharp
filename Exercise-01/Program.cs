57e0a67848ca88a9ba9464d7e1a3c2a633433c20
yashweybee <yashparmar@weybee.in>
2023-10-03 15:18:03 +0530

    Add project files.

---
+++ Exercise-01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c1 = 'A';
+
+            //Console.WriteLine((char)(64));
+
+
+
+            for (int i = 1; i <= 5; i++)
+            {
+                c1 = 'A';
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(c1);
+                    c1 = (char)(c1 + 1);
+                }
+                Console.WriteLine();
+            }
+
+            for(int i = 1; i <= 5; i++)
+            {
+                //c2 = '';
+                c1 = (char)(c1 - 1);
+                Console.Write(c1);
+
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
